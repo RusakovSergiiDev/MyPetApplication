734be81dda4b3407502b32b3d3ec4d07595dc7c8
Rusakov Serhii <rusakov.sergii.dev@gmail.com>
2024-01-13 23:13:15 +0100

    add new module

--- app/src/main/java/com/example/mypetapplication/App.kt
+++ app/src/main/java/com/example/mypetapplication/App.kt
@@ -1,15 +1,7 @@
 package com.example.mypetapplication
 
 import android.app.Application
-import com.example.mypetapplication.dagger.AppComponent
-import com.example.mypetapplication.dagger.DaggerAppComponent
 
 class App : Application() {
 
-    lateinit var appComponent: AppComponent
-
-    override fun onCreate() {
-        super.onCreate()
-        appComponent = DaggerAppComponent.factory().create(this)
-    }
 }
---
+++ domainModule/src/main/java/com/example/domainmodule/ContentRepository.kt
@@ -0,0 +1,4 @@
+package com.example.domainmodule
+
+class ContentRepository {
+}
