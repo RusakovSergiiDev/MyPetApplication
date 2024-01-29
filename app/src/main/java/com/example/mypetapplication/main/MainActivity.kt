4f4a6ef76944c8dd53f47f91caa6013587b3f753
Rusakov Serhii <rusakov.sergii.dev@gmail.com>
2024-01-29 20:51:25 +0100

    Implement detekt

--- app/src/main/java/com/example/mypetapplication/main/MainActivity.kt
+++ app/src/main/java/com/example/mypetapplication/main/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.mypetapplication.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import com.example.mypetapplication.MyPetApplicationService
 import com.example.mypetapplication.R
 import com.example.mypetapplication.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
@@ -11,5 +15,12 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        findViewById<View>(R.id.start).setOnClickListener {
+            startService(Intent(this, MyPetApplicationService::class.java))
+        }
+        findViewById<View>(R.id.end).setOnClickListener {
+            stopService(Intent(this, MyPetApplicationService::class.java))
+        }
     }
 }
